44a9f9d8cab08c629c01cb9993d2948ddcbe40e2
Sergey Tarasov <tarasov.inf@gmail.com>
2010-03-19 23:45:31 +0100

    Updated generic mapper and manager functionality and intergration. Updated generic mapper and manager unit tests. Added stubs for some of the mojo's utility functions not resolved by IDE.

--- mercurius/tests/models/genericMapperTest.js
+++ mercurius/tests/models/genericMapperTest.js
@@ -80,5 +80,22 @@ Models.GenericMapperTest = Class.create({
         Test.requireArraysEqual([null, null, 1], updateContext.params);
 
         return Mojo.Test.passed;
+    },
+
+    test_to_delete_sql_should_fail_when_non_numeric_id_passed: function() {
+        Test.requireException((function() {
+            this._mapper.toDeleteSql("1");
+        }).bind(this));
+
+        return Mojo.Test.passed;
+    },
+
+    test_to_delete_sql_should_return_valid_delete_statement: function() {
+        var deleteContext = this._mapper.toDeleteSql(1);
+
+        Mojo.requireEqual("DELETE FROM name_and_flag WHERE id=?;", deleteContext.sql);
+        Test.requireArraysEqual([1], deleteContext.params);
+
+        return Mojo.Test.passed;
     }
 });
