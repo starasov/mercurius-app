9f625629591c18df45f8a1845c8001e54e4dc0c3
Sergey Tarasov <tarasov.inf@gmail.com>
2010-06-03 21:07:56 +0300

    - Migrated database service on Prototype Class - Added "personal" logger

--- mercurius/app/database/service.js
+++ mercurius/app/database/service.js
@@ -1,95 +1,92 @@
-// ToDO: migrate from prototype based class creation to Class.create({}) form.
+Database.Service = Class.create({
+    log: Utils.NullLog,
 
-Database.Service = function(name, version, displayName, size) {
-    Mojo.Log.info("[DbService.initialize] - begin");
+    initialize: function(name, version, displayName, size) {
+        this.log.info("[initialize] - begin");
 
-    Mojo.requireString(name, "'name' parameter is required");
-    Mojo.requireString(version, "'version' parameter is required");
-    Mojo.requireString(displayName, "'displayName' parameter is required");
-    Mojo.requireNumber(size, "'size' parameter is required");
+        Mojo.requireString(name, "'name' parameter is required");
+        Mojo.requireString(version, "'version' parameter is required");
+        Mojo.requireString(displayName, "'displayName' parameter is required");
+        Mojo.requireNumber(size, "'size' parameter is required");
 
-    this._name = name;
-    this._version  = version;
-    this._displayName = displayName;
-    this._size = size;
-    this._db = null;
+        this._name = name;
+        this._version  = version;
+        this._displayName = displayName;
+        this._size = size;
+        this._db = null;
 
-    Mojo.Log.info("[DbService.initialize] - end");
-};
+        this.log.info("[initialize] - end");
+    },
 
-Database.Service.prototype.setVersionProvider = function(versionProvider) {
-    this._versionProvider = versionProvider;
-};
+    setVersionProvider: function(versionProvider) {
+        this._versionProvider = versionProvider;
+    },
 
-Database.Service.prototype.setDatabaseInitializer = function(databaseInitializer) {
-    this._databaseInitializer = databaseInitializer;
-};
+    setDatabaseInitializer: function(databaseInitializer) {
+        this._databaseInitializer = databaseInitializer;
+    },
 
-Database.Service.prototype.getDatabase = function(successHandler, errorHandler) {
-    if (this.isOpened()) {
-        successHandler(this._db);
-    } else {
-        this.open(successHandler, errorHandler);
-    }
-};
-
-Database.Service.prototype.open = function(successHandler, errorHandler) {
-    Mojo.Log.info("[Database.Service.open] - begin");
-
-    Mojo.require(this._databaseInitializer, "Database initializer instance should be set before 'open' method called.");
-    Mojo.require(this._versionProvider, "Database version provider instance should be set before 'open' method called.");
-
-    var clientSuccessHandler = successHandler || Prototype.emptyFunction;
-    var clientErrorHandler = errorHandler || Prototype.emptyFunction;
-
-    try {
-        Mojo.Log.info("[Database.Service.open] - opening database...");
-        this._db = openDatabase(this._name, "0.0", this._displayName, this._size);
-        if (!this._db) {
-            Mojo.Log.error("[DbService.open] - failed to open database!");
-            clientErrorHandler();
+    getDatabase: function(successHandler, errorHandler) {
+        if (this.isOpened()) {
+            successHandler(this._db);
         } else {
-            this.processSuccessOpen(clientSuccessHandler, clientErrorHandler);
+            this.open(successHandler, errorHandler);
         }
-    } catch(e) {
-        Mojo.Log.error("[DbService.open] - failed to open database - %s", e);
-        clientErrorHandler(e);
+    },
+
+    open: function(successHandler, errorHandler) {
+        this.log.info("[Database.Service.open] - begin");
+
+        Mojo.require(this._databaseInitializer, "Database initializer instance should be set before 'open' method called.");
+        Mojo.require(this._versionProvider, "Database version provider instance should be set before 'open' method called.");
+
+        var clientSuccessHandler = successHandler || Prototype.emptyFunction;
+        var clientErrorHandler = errorHandler || Prototype.emptyFunction;
+
+        try {
+            this.log.info("[Database.Service.open] - opening database...");
+            this._db = openDatabase(this._name, "0.0", this._displayName, this._size);
+            if (!this._db) {
+                this.log.error("[DbService.open] - failed to open database!");
+                clientErrorHandler();
+            } else {
+                this.processSuccessOpen(clientSuccessHandler, clientErrorHandler);
+            }
+        } catch(e) {
+            this.log.error("[DbService.open] - failed to open database - %s", e);
+            clientErrorHandler(e);
+        }
+
+        this.log.info("[Database.Service.open] - end");
+    },
+
+    isOpened: function() {
+        return this._db != null;
+    },
+
+    processSuccessOpen: function processSuccessOpen(successHandler, errorHandler) {
+        this.log.info("[DbService.processSuccessOpen] - end");
+
+        if (this._versionProvider.hasCurrentVersion() && this._version != this._versionProvider.getCurrentVersion()) {
+            this.log.info("[DbService.processSuccessOpen] - database version change detected from '%s' to '%s'", this._version, this._versionProvider.getCurrentVersion());
+            this.handleDatabaseUpdate();
+        } else if (!this._versionProvider.hasCurrentVersion()) {
+            this.log.info("[DbService.processSuccessOpen] - database has not been previously created");
+            this.handleDatabaseCreation(successHandler, errorHandler);
+        }
+
+        this.log.info("[DbService.processSuccessOpen] - end");
+    },
+
+    handleDatabaseCreation: function(clientSuccessHandler, clientErrorHandler) {
+        this._databaseInitializer.initializeDatabase(this._db,
+                (function() { clientSuccessHandler(this._db); }).bind(this),
+                clientErrorHandler
+        );
+    },
+
+    handleDatabaseUpdate: function() {
+        Mojo.require(false, "Not implemented!");
     }
+});
 
-    Mojo.Log.info("[Database.Service.open] - end");
-};
-
-
-Database.Service.prototype.isOpened = function() {
-    return this._db != null;
-};
-
-
-/* @private */
-Database.Service.prototype.processSuccessOpen = function processSuccessOpen(successHandler, errorHandler) {
-    Mojo.Log.info("[DbService.processSuccessOpen] - end");
-
-    if (this._versionProvider.hasCurrentVersion() && this._version != this._versionProvider.getCurrentVersion()) {
-        Mojo.Log.info("[DbService.processSuccessOpen] - database version change detected from '%s' to '%s'", this._version, this._versionProvider.getCurrentVersion());
-        this.handleDatabaseUpdate();
-    } else if (!this._versionProvider.hasCurrentVersion()) {
-        Mojo.Log.info("[DbService.processSuccessOpen] - database has not been previously created");
-        this.handleDatabaseCreation(successHandler, errorHandler);
-    }
-
-    Mojo.Log.info("[DbService.processSuccessOpen] - end");
-};
-
-
-/* @private */
-Database.Service.prototype.handleDatabaseCreation = function handleDatabaseCreation(clientSuccessHandler, clientErrorHandler) {
-    this._databaseInitializer.initialize(this._db,
-            (function() { clientSuccessHandler(this._db); }).bind(this),
-            clientErrorHandler
-    );
-};
-
-/* @private */
-Database.Service.prototype.handleDatabaseUpdate = function handleDatabaseUpdate() {
-    Mojo.require(false, "Not implemented!");
-};
