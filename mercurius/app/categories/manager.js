6de0beba163cde962e9f7fa36516ceafc1f698d5
Sergey Tarasov <tarasov.inf@gmail.com>
2010-07-29 07:19:31 +0300

    - Added categories manager integration tests

--- mercurius/app/categories/manager.js
+++ mercurius/app/categories/manager.js
@@ -1,15 +1,17 @@
 Categories.TOP = 0;
 
 Categories.Manager = Class.create(Models.GenericManager, {
-    findTopCategories: function(extraParameters, successCallback, errorCallback) {
-        this.find({parent_id: 0}, extraParameters, successCallback, errorCallback);
-    },
-
     findTopCategoriesByType: function(type, extraParameters, successCallback, errorCallback) {
+        Mojo.requireFunction(successCallback, "'successCallback' should be a function.");
+        Mojo.requireFunction(errorCallback, "'errorCallback' should be a function.");
+
         this.find({parent_id: 0, type: type}, extraParameters, successCallback, errorCallback);
     },
 
     findChildCategories: function(parentId, extraParameters, successCallback, errorCallback) {
+        Mojo.requireFunction(successCallback, "'successCallback' should be a function.");
+        Mojo.requireFunction(errorCallback, "'errorCallback' should be a function.");
+
         this.find({parent_id: parentId}, extraParameters, successCallback, errorCallback);
     }
 });
